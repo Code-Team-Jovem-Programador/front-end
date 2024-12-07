f7ed3d9a45c508d6fe9b88f5e8899d6db5056bc6
Ana <analuizzabritto@gmail.com>
2024-12-07 18:41:06 -0300

    Tela de cadastro

--- src/components/RegisterForm.js
+++ src/components/RegisterForm.js
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import Swal from 'sweetalert2'; // Importa o SweetAlert2
+import './RegisterForm.css'; // Importa os estilos
+import { useNavigate } from 'react-router-dom';
 
 const RegisterForm = () => {
   const [formData, setFormData] = useState({
@@ -7,8 +10,7 @@ const RegisterForm = () => {
     password: '',
     email: '',
   });
-  const [error, setError] = useState('');
-  const [success, setSuccess] = useState('');
+  const navigate = useNavigate();
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -19,50 +21,82 @@ const RegisterForm = () => {
     e.preventDefault();
 
     try {
-      const response = await axios.post('https://gerenciador-estoque-prod.onrender.com/api/register/', formData);
-      setSuccess('Usuário cadastrado com sucesso!');
-      setError('');
+      await axios.post('https://gerenciador-estoque-prod.onrender.com/api/register/', formData);
+
+      // Alerta de sucesso com SweetAlert2
+      Swal.fire({
+        title: 'Sucesso!',
+        text: 'Usuário cadastrado com sucesso!',
+        icon: 'success',
+        confirmButtonText: 'OK',
+        confirmButtonColor: '#631E4D',
+      });
+
+      setTimeout(() => navigate('/login'), 2000); // Redireciona para o login após sucesso
     } catch (error) {
-      setError('Erro ao cadastrar o usuário. Verifique os dados informados.');
-      setSuccess('');
+      // Alerta de erro com SweetAlert2
+      Swal.fire({
+        title: 'Erro!',
+        text: 'Erro ao cadastrar o usuário. Verifique os dados informados.',
+        icon: 'error',
+        confirmButtonText: 'Tentar novamente',
+        confirmButtonColor: '#631E4D',
+      });
     }
   };
 
+  const handleGoBack = () => {
+    navigate(-1)
+  }
+
   return (
     <div className="register-container">
-      <form onSubmit={handleRegister} className="register-form">
-        <h3>Cadastre-se</h3>
-        {error && <p className="error-message">{error}</p>}
-        {success && <p className="success-message">{success}</p>}
-        <label htmlFor="username">Usuário</label>
-        <input
-          type="text"
-          id="username"
-          name="username"
-          value={formData.username}
-          onChange={handleChange}
-          required
+      <div className="register-left">
+        <img
+          src="/assets/skeleton.png"
+          alt="Skeleton Illustration"
+          className="register-image"
         />
-        <label htmlFor="password">Senha</label>
-        <input
-          type="password"
-          id="password"
-          name="password"
-          value={formData.password}
-          onChange={handleChange}
-          required
-        />
-        <label htmlFor="email">Email</label>
-        <input
-          type="email"
-          id="email"
-          name="email"
-          value={formData.email}
-          onChange={handleChange}
-          required
-        />
-        <button type="submit" className="register-button">Cadastrar</button>
-      </form>
+        <div className="register-slogan">
+          <h2>Code Team</h2>
+          <p>Sistema de Gerenciamento de Estoque</p>
+        </div>
+      </div>
+      <div className="register-right">
+        <form onSubmit={handleRegister} className="register-form">
+          <h3>Cadastre sua nova conta</h3>
+          <p className="register-subtitle">Entre no nosso sistema e gerencie seu negócio</p>
+          <label htmlFor="username">Usuário</label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            value={formData.username}
+            onChange={handleChange}
+            required
+          />
+          <label htmlFor="password">Senha</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            required
+          />
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+          <button type="submit" className="register-button">Cadastrar</button>
+          <a href= "#" onClick={handleGoBack} className='back-link'>Voltar</a>
+        </form>
+        </div>
     </div>
   );
 };
